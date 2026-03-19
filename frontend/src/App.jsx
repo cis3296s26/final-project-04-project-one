e8beadc7b9206cb9b764868795b4cd46e6bb2667
zackaidss <tuu07612@temple.edu>
2026-03-19 18:26:22 -0400

    game mode buttons created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import MainMenu from './components/MainMenu'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-radial from-green-500 to-green-700">
+    <div className="min-h-screen min-w-full bg-radial from-green-500 to-green-700 content-center">
       <MainMenu />
     </div>
   )
--- frontend/src/components/MainMenu.jsx
+++ frontend/src/components/MainMenu.jsx
@@ -1,5 +1,19 @@
 export default function MainMenu() {
     return (
-        <div>Hello World</div>
+        <div className=''>
+            <div className='columns-3'>
+                <section className=''>
+                    <button className="w-full h-40 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
+                        Crazy Eights
+                    </button>
+                    <button className="w-full h-40 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
+                        Uno
+                    </button>
+                    <button className="w-full h-40 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
+                        Stacking
+                    </button>
+                </section>
+            </div>
+        </div>
     )
 }
