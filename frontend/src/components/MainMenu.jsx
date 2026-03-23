ff2602b86c78ab91745c9b25e486aa32bf157823
zackaidss <tuu07612@temple.edu>
2026-03-23 18:46:40 -0400

    profile picture and settings buttons added

--- frontend/src/components/MainMenu.jsx
+++ frontend/src/components/MainMenu.jsx
@@ -1,16 +1,29 @@
+import setting_gear from '../assets/setting_gear.png'
+import placeholder_pfp from '../assets/placeholder_pfp.png'
+
 export default function MainMenu() {
     return (
         <div className=''>
-            <div className='columns-3'>
-                <section className=''>
-                    <button className="w-full h-40 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
+            <div className='mx-50'>
+                <section className='columns-3 m-2'>
+                    <button className="w-full h-90 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
                         Crazy Eights
                     </button>
-                    <button className="w-full h-40 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
+                    <button className="w-full h-90 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
                         Uno
                     </button>
-                    <button className="w-full h-40 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
+                    <button className="w-full h-90 bg-white rounded-2xl text-black text-3xl font-bold hover:bg-gray-500 transition-all">
                         Stacking
+                    </button>
+                </section>
+                <section className='flex justify-between'>
+                    <button className='w-60 h-20 bg-white flex justify-between'>
+                        <img src={placeholder_pfp} className='h-20 w-20 rounded-full object-cover'></img>
+                        <span className='text-2xl font-semibold'>John Doe</span>
+                        <span>level</span>
+                    </button>
+                    <button className='w-30 h-30'>
+                        <img src={setting_gear}></img>
                     </button>
                 </section>
             </div>
