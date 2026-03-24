3329cbd6401ec264b510869003da60a07359b76b
zackaidss <tuu07612@temple.edu>
2026-03-24 19:14:54 -0400

    added buttons for ModeSelect

--- frontend/src/components/ModeSelect.jsx
+++ frontend/src/components/ModeSelect.jsx
@@ -14,6 +14,21 @@ export default function ModeSelect() {
                     <img src={back} className='m-5 w-16 h-16 cursor-pointer hover:w-20 h-20'></img>
                 </button>
             </section>
+            <section className='grid grid-rows-4 justify-center gap-2'>
+                <button className='bg-white w-full h-20'>
+                    vs. Bots
+                </button>
+                <button className='bg-white w-full h-20'>
+                    Public
+                </button>
+                <button className='bg-white w-full h-20'>
+                    Private
+                </button>
+                <div className='w-full h-20'>
+                    <input type='text' placeholder='Room Code'/>
+                    <button className='bg-white'>join</button>
+                </div>
+            </section>
         </div>
     )
 }
