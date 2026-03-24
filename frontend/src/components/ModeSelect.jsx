cb8a884a5d21e7e2dd7ee3ed84d2ba48261345c4
zackaidss <tuu07612@temple.edu>
2026-03-24 19:02:22 -0400

    created back button

--- frontend/src/components/ModeSelect.jsx
+++ frontend/src/components/ModeSelect.jsx
@@ -1,7 +1,19 @@
+import back from '../assets/back.svg'
+import { useNavigate } from 'react-router-dom';
+
 export default function ModeSelect() {
+    const navigate = useNavigate();
+    const goToHome = () => {
+        navigate('/');
+    }
+
     return (
         <div>
-            testy westy
+            <section>
+                <button onClick={goToHome} className=' '>
+                    <img src={back} className='m-5 w-16 h-16 cursor-pointer hover:w-20 h-20'></img>
+                </button>
+            </section>
         </div>
     )
 }
