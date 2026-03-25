5615d8f72218edb5b87de31aee47f9a280ec38d7
zackaidss <tuu07612@temple.edu>
2026-03-25 00:54:42 -0400

    added bottom icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="min-h-screen min-w-full bg-radial from-green-500 to-green-700 ">
+    <div className="min-h-screen min-w-full bg-radial from-green-500 to-green-700">
       <Routes>
         <Route path="/" element={<MainMenu />} />
         <Route path="mode-select" element={<ModeSelect />} />
--- frontend/src/components/ModeSelect.jsx
+++ frontend/src/components/ModeSelect.jsx
@@ -1,4 +1,8 @@
 import back from '../assets/back.svg'
+import people from '../assets/people.svg'
+import cardDraw from '../assets/card-draw.svg'
+import cardHand from '../assets/card-hand.svg'
+import timer from '../assets/timer.svg'
 import { useNavigate } from 'react-router-dom';
 
 export default function ModeSelect() {
@@ -8,25 +12,41 @@ export default function ModeSelect() {
     }
 
     return (
-        <div>
-            <section>
-                <button onClick={goToHome} className=' '>
-                    <img src={back} className='m-5 w-16 h-16 cursor-pointer hover:w-20 h-20'></img>
-                </button>
+        <div className='flex flex-col min-h-screen'>
+            <section className='flex-grow'>
+                <section>
+                    <button onClick={goToHome} className=' '>
+                        <img src={back} className='m-5 w-16 h-16 cursor-pointer hover:w-20 h-20'></img>
+                    </button>
+                </section>
+                <section className='grid grid-rows-4 justify-center gap-2'>
+                    <button className='bg-white w-full h-20'>
+                        vs. Bots
+                    </button>
+                    <button className='bg-white w-full h-20'>
+                        Public
+                    </button>
+                    <button className='bg-white w-full h-20'>
+                        Private
+                    </button>
+                    <div className='w-full h-20'>
+                        <input type='text' placeholder='Room Code'/>
+                        <button className='bg-white'>join</button>
+                    </div>
+                </section>
             </section>
-            <section className='grid grid-rows-4 justify-center gap-2'>
-                <button className='bg-white w-full h-20'>
-                    vs. Bots
-                </button>
-                <button className='bg-white w-full h-20'>
-                    Public
-                </button>
-                <button className='bg-white w-full h-20'>
-                    Private
-                </button>
-                <div className='w-full h-20'>
-                    <input type='text' placeholder='Room Code'/>
-                    <button className='bg-white'>join</button>
+            <section className='grid grid-cols-4 mt-auto'>
+                <div className='w-16 h-16'>
+                    <img src={people}></img>
+                </div>
+                <div className='w-16 h-16'>
+                    <img src={cardDraw}></img>
+                </div>
+                <div className='w-16 h-16'>
+                    <img src={cardHand}></img>
+                </div>
+                <div className='w-16 h-16'>
+                    <img src={timer}></img>
                 </div>
             </section>
         </div>
