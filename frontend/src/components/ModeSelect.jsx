bf6aa870d4dd44a451c9da4db744547f558884be
zackaidss <tuu07612@temple.edu>
2026-03-25 16:14:01 -0400

    bottom buttons done

--- frontend/src/components/ModeSelect.jsx
+++ frontend/src/components/ModeSelect.jsx
@@ -3,6 +3,7 @@ import people from '../assets/people.svg'
 import cardDraw from '../assets/card-draw.svg'
 import cardHand from '../assets/card-hand.svg'
 import timer from '../assets/timer.svg'
+import arrow from '../assets/arrow.svg'
 import { useNavigate } from 'react-router-dom';
 
 export default function ModeSelect() {
@@ -20,33 +21,69 @@ export default function ModeSelect() {
                     </button>
                 </section>
                 <section className='grid grid-rows-4 justify-center gap-2'>
-                    <button className='bg-white w-full h-20'>
+                    <button className='bg-white w-full h-20 hover:bg-gray-500'>
                         vs. Bots
                     </button>
-                    <button className='bg-white w-full h-20'>
+                    <button className='bg-white w-full h-20 hover:bg-gray-500'>
                         Public
                     </button>
-                    <button className='bg-white w-full h-20'>
+                    <button className='bg-white w-full h-20 hover:bg-gray-500'>
                         Private
                     </button>
                     <div className='w-full h-20'>
                         <input type='text' placeholder='Room Code'/>
-                        <button className='bg-white'>join</button>
+                        <button className='bg-white hover:bg-gray-500'>join</button>
                     </div>
                 </section>
             </section>
-            <section className='grid grid-cols-4 mt-auto'>
-                <div className='w-16 h-16'>
-                    <img src={people}></img>
+            <section className='grid grid-cols-4 mt-auto p-5 bg-gray-300'>
+                <div className='flex flex-row items-center gap-5'>
+                    <img src={people} className='w-16 h-16' />
+                    <span className='text-3xl font-bold'>5</span>
+                    <div className='flex flex-col gap-2'>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer'>
+                            <img src={arrow} className='w-7 h-7' />
+                        </button>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer'>
+                            <img src={arrow} className='w-7 h-7 rotate-180' />
+                        </button>
+                    </div>
                 </div>
-                <div className='w-16 h-16'>
-                    <img src={cardDraw}></img>
+                <div className='flex flex-row items-center gap-5'>
+                    <img src={cardDraw} className='w-16 h-16' />
+                    <span className='text-3xl font-bold'>3</span>
+                    <div className='flex flex-col gap-2'>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer'>
+                            <img src={arrow} className='w-7 h-7' />
+                        </button>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer'>
+                            <img src={arrow} className='w-7 h-7 rotate-180' />
+                        </button>
+                    </div>
                 </div>
-                <div className='w-16 h-16'>
-                    <img src={cardHand}></img>
+                <div className='flex flex-row items-center gap-5'>
+                    <img src={cardHand} className='w-16 h-16' />
+                    <span className='text-3xl font-bold'>7</span>
+                    <div className='flex flex-col gap-2'>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer '>
+                            <img src={arrow} className='w-7 h-7' />
+                        </button>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer '>
+                            <img src={arrow} className='w-7 h-7 rotate-180' />
+                        </button>
+                    </div>
                 </div>
-                <div className='w-16 h-16'>
-                    <img src={timer}></img>
+                <div className='flex flex-row items-center gap-5'>
+                    <img src={timer} className='w-16 h-16' />
+                    <span className='text-3xl font-bold'>30</span>
+                    <div className='flex flex-col gap-2'>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer'>
+                            <img src={arrow} className='w-7 h-7' />
+                        </button>
+                        <button className='w-7 h-7 flex items-center justify-center cursor-pointer'>
+                            <img src={arrow} className='w-7 h-7 rotate-180' />
+                        </button>
+                    </div>
                 </div>
             </section>
         </div>
