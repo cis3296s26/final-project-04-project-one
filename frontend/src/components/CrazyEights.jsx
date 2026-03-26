9f3d0d4d6cdaf3cbfa78fc484bbbf019b31dac46
zackaidss <tuu07612@temple.edu>
2026-03-26 18:50:11 -0400

    added one of the hands

--- frontend/src/components/CrazyEights.jsx
+++ frontend/src/components/CrazyEights.jsx
@@ -1,5 +1,16 @@
+import cardBack from '../assets/cards/card_back.png';
+
 export default function CrazyEights() {
     return (
-        <div>hello war</div>
+        <div>
+            <section className='flex flex-col items-start -space-y-60'>
+                <img src={cardBack} className='w-35 h-50 rotate-90'>
+                </img>
+                <img src={cardBack} className='w-35 h-50 rotate-90'>
+                </img>
+                <img src={cardBack} className='w-35 h-50 rotate-90'>
+                </img>
+            </section>
+        </div>
     )
 }
