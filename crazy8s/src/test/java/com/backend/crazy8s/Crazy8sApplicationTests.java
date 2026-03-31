925e9d51385aa0dd11da62fc30876f93884fd61a
zackaidss <tuu07612@temple.edu>
2026-03-31 16:16:25 -0400

    Created Maven Project and moved files

---
+++ crazy8s/src/test/java/com/backend/crazy8s/Crazy8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.crazy8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crazy8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
