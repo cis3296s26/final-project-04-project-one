ce046fcd381e9ef705cdd4e1292b46d1231b82db
zackaidss <tuu07612@temple.edu>
2026-04-01 10:20:40 -0400

    created newGame() function

--- frontend/src/api/gameApi.js
+++ frontend/src/api/gameApi.js
@@ -8,3 +8,27 @@ function makeDeck() {
             deck.push({ rank, suit });
     return deck.sort(() => Math.random() - 0.5);
 }
+
+export async function newGame() {
+    const deck = makeDeck();
+    const hands = [[], [], [], []];
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 7; j++)
+            hands[i].push(deck.pop());
+
+    const topCard = deck.pop();
+
+    return {
+        gameId: "mock-id",
+        hands,
+        deck,
+        discardPile: [topCard],
+        currentSuit: topCard.suit,
+        currentPlayer: 0,
+        direction: 1,
+        skipNext: false,
+        penaltyDraw: 0,
+        status: "IN_PROGRESS",
+        winner: null,
+    };
+}
