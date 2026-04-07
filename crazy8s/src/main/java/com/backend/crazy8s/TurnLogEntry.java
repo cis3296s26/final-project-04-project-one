d2c93b9d8c8ce8a407bc5c378b4a43d5dab3da90
zackaidss <tuu07612@temple.edu>
2026-04-07 15:41:50 -0400

    set up TurnLogEntry class

--- crazy8s/src/main/java/com/backend/crazy8s/TurnLogEntry.java
+++ crazy8s/src/main/java/com/backend/crazy8s/TurnLogEntry.java
@@ -1,5 +1,15 @@
+// TurnLogEntry.java
 package com.backend.crazy8s;
 
 public class TurnLogEntry {
-    
+    private String message;
+    private Card cardPlayed; // null if drew a card
+
+    public TurnLogEntry(String message, Card cardPlayed) {
+        this.message = message;
+        this.cardPlayed = cardPlayed;
+    }
+
+    public String getMessage() { return message; }
+    public Card getCardPlayed() { return cardPlayed; }
 }
