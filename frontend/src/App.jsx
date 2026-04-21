6951b1e578ffaed4f50a84cbb4b4ba2c667c0160
zackaidss <tuu07612@temple.edu>
2026-04-21 16:26:48 -0400

    modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css';
-import MainMenu from './components/MainMenu';
-import ModeSelect from './components/ModeSelect';
-import CrazyEights from './components/CrazyEights';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import MainMenu from "./components/MainMenu";
+import ModeSelect from "./components/ModeSelect";
+import WaitingRoom from "./components/WaitingRoom";
+import CrazyEights from "./components/CrazyEights";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="min-h-screen min-w-full bg-radial from-green-500 to-green-700">
       <Routes>
         <Route path="/" element={<MainMenu />} />
-        <Route path="mode-select" element={<ModeSelect />} />
-        <Route path="/crazy-eights" element={<CrazyEights />}/>
+        <Route path="/mode-select" element={<ModeSelect />} />
+        <Route path="/waiting-room" element={<WaitingRoom />} />
+        <Route path="/crazy-eights" element={<CrazyEights />} />
       </Routes>
     </div>
-  )
+  );
 }
-
-
