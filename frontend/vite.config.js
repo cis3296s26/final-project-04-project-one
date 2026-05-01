077ad4043ba43621a00670ac628c215edeab85a4
bmo . <tul53575@temple.edu>
2026-05-01 16:48:36 -0400

    Installable PWA without backend functionality
    -Attempt to use Vite Plugin PWA (VPP)
    -Modify vite.config.js to configure VPP service worker caching & manifest
    -Add a couple of icons tested working on Windows (npm run build & preview)
    -Ask me for resources used if confused

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,92 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/final-project-04-project-one\.onrender\.com\/api\/game\/.*$/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 // 24 hours
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+
+          {
+            urlPattern: /\.(?:png|svg)$/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+              }
+            }
+          },
+
+          {
+            urlPattern: /\.(?:js|css)$/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources-cache'
+            }
+          }
+        ]
+      },
+
+      includeAssets: ['favicon.svg'],
+
+      manifest: {
+        name: 'Project One',
+        short_name: 'P1',
+        description: 'Card-shedding game of the future',
+        theme_color: '#ffffff',
+        background_color: '#45b054',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'icon512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon512.svg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'icon192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon192.svg',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   define: {
     global: 'window',
   },
