13bfb16ad7c5ea5a55716dc539003f4a29954385
bmo . <tul53575@temple.edu>
2026-05-01 23:25:18 -0400

    Add Vite Preview Origin to Game Controller

--- crazy8s/src/main/java/com/backend/crazy8s/GameController.java
+++ crazy8s/src/main/java/com/backend/crazy8s/GameController.java
@@ -1,14 +1,18 @@
 package com.backend.crazy8s;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/game")
 @CrossOrigin(origins = {
     "http://localhost:5173",
+    "http://localhost:4173",
     "https://final-project-04-project-one.onrender.com"
 })
 public class GameController {
